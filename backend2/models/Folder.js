2eea5800a19b6b2d0a0a38d0004b14c87723cd57
Prashant Parshuramkar <110542623+PrashantPKP@users.noreply.github.com>
2025-07-04 16:47:42 +0530

    Delete backend2 directory

--- backend2/models/Folder.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const folderSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-const Folder = mongoose.model("Folder", folderSchema);
-module.exports = Folder;
